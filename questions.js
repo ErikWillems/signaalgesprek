ffef3f67d8f8e00c2c199e3ef1e82d51aa8860a2
Erik Willems <webmaster@rocketspeed.nl>
2025-11-07 10:57:09 +0100

    stuff

--- questions.js
+++ questions.js
@@ -10,45 +10,55 @@ const digestQuestions = function () {
           .append("div")
           .attr("class", "question")
           .each(function (question) {
-            console.log("ENTER!!");
             question.selected = null;
             const elem = d3
               .select(this)
               .append("div")
               .attr("class", "question-card p-5 m-1 shadow");
-
-            elem.append("h3").text(question.label).attr("class", "mt-0 mb-4");
-            elem
-              .append("div")
-              .attr("class", "answers")
-              .selectAll(".answer")
-              .data(question.answers)
-              .join(
-                function (enter) {
-                  const elem = enter
-                    .append("div")
-                    .attr("class", "answer")
-                    .each(function (answer, answerIndex) {
-                      const elem = d3.select(this);
-                      const label = elem.append("label");
-                      label
-                        .append("input")
-                        .attr("type", "radio")
-                        .on("input", function () {
-                          question.selected = answerIndex;
-                          digestQuestions();
-                        });
-                      label
-                        .append("span")
-                        .attr("class", "ms-4")
-                        .text(answer.label);
-                    });
-                },
-                function (update) {},
-                function (exit) {
-                  exit.remove();
-                }
-              );
+            if (question.type === "intro") {
+              elem.append("p").text(question.label).attr("class", "mt-0 mb-4");
+              elem
+                .append("button")
+                .attr("class", "btn btn-lg btn-light")
+                .text("Start de test!")
+                .on("click", function () {
+                  question.selected = -1;
+                  digestQuestions();
+                });
+            } else {
+              elem.append("h3").text(question.label).attr("class", "mt-0 mb-4");
+              elem
+                .append("div")
+                .attr("class", "answers")
+                .selectAll(".answer")
+                .data(question.answers)
+                .join(
+                  function (enter) {
+                    const elem = enter
+                      .append("div")
+                      .attr("class", "answer")
+                      .each(function (answer, answerIndex) {
+                        const elem = d3.select(this);
+                        const label = elem.append("label");
+                        label
+                          .append("input")
+                          .attr("type", "radio")
+                          .on("input", function () {
+                            question.selected = answerIndex;
+                            digestQuestions();
+                          });
+                        label
+                          .append("span")
+                          .attr("class", "ms-4")
+                          .text(answer.label);
+                      });
+                  },
+                  function (update) {},
+                  function (exit) {
+                    exit.remove();
+                  }
+                );
+            }
           });
       },
       function (update) {
